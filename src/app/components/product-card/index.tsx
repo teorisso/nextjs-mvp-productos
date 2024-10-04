472dda69d5988e46ad53ca47df5ad1f14c3bbef4
Teo Risso <68182473+teorisso@users.noreply.github.com>
2024-10-04 16:10:10 -0300

    botones en la ruleta

--- src/app/components/product-card/index.tsx
+++ src/app/components/product-card/index.tsx
@@ -12,12 +12,24 @@ interface ProductCardProps {
 export default function ProductCard({products,}: {products: ProductCardProps[];}) {
     const router = useRouter();
 
-    const [state, setState] = useState();
+    const [currentIndex, setCurrentIndex] = useState(0);
 
+    function handleNext() {
+        setCurrentIndex(currentIndex + 1);
+    }
+
+    function handlePrev() {
+        const isFirst = currentIndex === 0;
+        setCurrentIndex(isFirst ? products.length - 1 :currentIndex - 1);
+    }
+
+    const nextDisabled = currentIndex + 5 === products.length;
+    const prevDisabled = currentIndex === 0;
+    
     return (
         <div className='relative'>
         <div className="flex flex-wrap gap-4 mt-20">
-          {products.slice(0,5).map((product) => (
+          {products.slice(currentIndex, currentIndex+5).map((product) => (
             <div 
             key={product.id} 
             className="w-[200px] h-[200px] bg-gray-900 flex flex-col items-center justify-center cursor-pointer hover:bg-gray-300 transition-all"
@@ -31,10 +43,16 @@ export default function ProductCard({products,}: {products: ProductCardProps[];}
             </div>
             ))}
         </div>
-        <button className='text-4xl font font-semibold absolute top-[56%] -right-14 hover:text-gray-600'>
+        <button className='text-4xl font font-semibold absolute top-[56%] -right-14 hover:text-gray-600' 
+        onClick={()=>handleNext()}
+        disabled={nextDisabled}
+        >
             {'>'}
         </button>
-        <button className='text-4xl font font-semibold absolute top-[56%] -left-14 hover:text-gray-600'>
+        <button className='text-4xl font font-semibold absolute top-[56%] -left-14 hover:text-gray-600'
+         onClick={()=>handlePrev()}
+         disabled={prevDisabled}
+         >
             {'<'}
         </button>
         </div>
